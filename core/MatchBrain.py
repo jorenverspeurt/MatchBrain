f39d89bf3778e6eca32f269b7af183f9a6d2c81c
Joren Verspeurt <joren.verspeurt@student.kuleuven.be>
2015-09-23 15:53:30 +0200

    Dit is zo'n "oh nee mijn batterij is bijna plat en ik moet ergens anders voortwerken" commit

--- core/MatchBrain.py
+++ core/MatchBrain.py
@@ -7,6 +7,9 @@ from cocos.scene import Scene
 from cocos.layer import MultiplexLayer
 from Menus import MainMenu
 
+import logging
+from datetime import datetime
+
 def setup():
     director.director.init(width=800, height=700, caption='MatchBrain')
     scene = Scene()
@@ -22,8 +25,14 @@ def cleanup():
     pass
 
 if __name__ == '__main__':
+    rootLogger = logging.getLogger('')
+    rootLogger.setLevel(logging.DEBUG)
+    rootHandler = logging.StreamHandler()
+    rootFormatter =
+    jsonLogger = logging.getLogger('json')
+    jsonLogger.setLevel(logging.INFO)
+    jsonRootFormatter =
     pyglet.resource.path = ['../resources']
     pyglet.resource.reindex()
-    testIm = pyglet.resource.image('46.png')
     game(scene=setup())
     cleanup()
